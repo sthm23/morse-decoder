f54adf9322be3727ccd6bfcd3e63f94dc4542026
sthm23 <94516310+sthm23@users.noreply.github.com>
2022-01-25 17:49:21 +0500

    'feat: morse-decoded solution solved'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let obj_keys = Object.keys(MORSE_TABLE);
+    let obj_values = Object.values(MORSE_TABLE);
+    
+    //expr делим на 10 кусков
+    let arr = [];
+    for(let i=0; i<expr.length;){
+        let a = expr.slice(i, (i+10))
+        arr.push(a);
+        i=i+10
+    }
+ 
+    //массив из 10 кусков чисел преврашаем на точку и на чертечку 
+    const arr2 = arr.map(item=>{
+        let tempArr = [];
+        for(let i=0; i<item.length; i=i+2){
+            if(item[i]+item[i+1]==='10'){
+                tempArr.push('.');
+            }else if(item[i]+item[i+1]==='11'){
+                tempArr.push('-');
+            }else if(item[i]==='*'){
+                tempArr.push('*')
+            }
+        }
+        return tempArr.join("");
+    })
+
+    //массив морзе преврашаем на букву
+    let arr3 = [];
+    for(let q=0; q<arr2.length; q++){
+        if(arr2[q]==='*****'){
+            arr3.push(' ');
+        }else{
+            for(let i=0; i<obj_keys.length; i++){
+                if(arr2[q]==obj_keys[i]){
+                    arr3.push(obj_values[i])
+                }
+            }
+        }
+    }
+    return arr3.join('');
 }
 
 module.exports = {
